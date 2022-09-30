3d439423589737265094b967ebf0605d6cf7f247
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2022-09-30 19:04:26 +0300

    changes canceled

--- Thermals_finder/parse_xcontest/xcontest/takeoff_data.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-__all__ = ['Takeoff']
-
-
-class Takeoff(Enum):
-    KONCHINKA = dict(latitude=38.14592, longitude=54.66127)
-    KLIN = dict(latitude=36.58837, longitude=56.34938)
