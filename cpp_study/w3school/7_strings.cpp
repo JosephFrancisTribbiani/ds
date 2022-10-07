26453b785a713ae2c463e1e1e0f5cdb1a7282fb5
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2022-10-07 19:45:38 +0300

    strings and operators passed

---
+++ cpp_study/w3school/7_strings.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>  // для полноценной работы со строками необходимо добавить эту библиотеку
+using namespace std;
+
+int main() {
+	// создание первой строки
+	string greeting = "Hello";
+	cout << greeting << endl;
+
+	// конкатенация строк
+	string firstName = "John";
+	string lastName = "Doil";
+	cout << firstName + " " + lastName << endl;
+
+	// вообщето, строки это экземпляры классов, и они имеют свои методы
+	string fullName = firstName.append(" ");
+	fullName.append(lastName);
+	cout << fullName << endl;
+
+	// число и строку конкатенировать нельзя
+	// т.е. "20" + 10 выдаст ошибку
+
+	// длину строки можно определить с помощью методов length() или size()
+	string txt = "veryyyyloooongstring";
+	cout << "Using method length:\t" << txt.length() << endl;
+	cout << "Using method size:\t" << txt.size() << endl;
+
+	// to access string character by index use square quotes
+	cout << "Third char of very long string:\t" << txt[2] << endl;
+	// the first index begins from 0
+
+	// also you can change the single string char
+	txt[2] = 'A';
+	cout << "Third char of very long string:\t" << txt[2] << endl;
+
+	// special characters:
+	// \n - new line (example: "Hello\n")
+	// \t - tabular (example: "Answer:\t5")
+	// escape character for ', ", \ (example: "It\'s me")
+
+	// to get string entered by user you can use cin
+	// but space character (whitespaces, tabs etc.) terminates user input
+	// for example if you type "John Snow" you will get "Jonh" only
+	// 
+	// to parse entire string use getline() function (see example below)
+	cout << "Enter your full name:" << endl;
+	getline(cin, fullName);
+	cout << "Your name is:\t" << fullName << endl;
+	// results:
+	// Enter your full name:
+	// John Snow
+	// Your name is:	John Snow
+	return 0;
+}
--- cpp_study/w3school/Readme.md
+++ cpp_study/w3school/Readme.md
@@ -1,9 +1,10 @@
 В данном разделе представлены материалы, которые использовались для изучения C++ на портале <a href="https://www.w3schools.com/cpp/default.asp">w3chool</a>.
 
 Представлены следующие разделы (одноименно названные в соответствии с разделами в w3school):
-1. get_started
-2. output
-3. variables
-4. user_input
-5. data_types
-6. operators
+1. [get_started](./1_get_started.cpp)
+2. [output](./2_output.cpp)
+3. [variables](./3_variables.cpp)
+4. [user_input](./4_user_input.cpp)
+5. [data_types](./5_data_types.cpp)
+6. [operators](./6_operators.cpp)
+7. [strings](./7_strings.cpp)
