db234377e0c98bcb7feda7c1da633a6e8338de50
JosephFrancisTribbiani <alpex.promalp@gmail.com>
2022-10-14 09:34:36 +0300

    task 764 solved

---
+++ Informatics/Cources/Data_structures_and_algorithms/Data_structures/topic_3/task_764_balanced_tree.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from typing import Union
+import sys
+
+
+LOCAL_FILE = False
+
+
+class Node:
+    def __init__(self, key: Union[int, float]):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, key: Union[int, float]):
+        if not self.root:
+            self.root = Node(key=key)
+            return
+
+        node = self.root
+
+        while node:
+            if key < node.key:
+                if not node.left:
+                    node.left = Node(key=key)
+                    return
+                else:
+                    node = node.left
+            elif key > node.key:
+                if not node.right:
+                    node.right = Node(key=key)
+                    return
+                else:
+                    node = node.right
+            else:
+                return
+        return
+
+    def check_balance(self):
+        if self.root:
+            self._check_balance(node=self.root)
+        return
+
+    def _check_balance(self, node):
+        if node:
+            l_depth = self._check_balance(node=node.left)
+            r_depth = self._check_balance(node=node.right)
+            if abs(l_depth - r_depth) > 1:
+                print("NO")
+                sys.exit(0)
+            return max(l_depth, r_depth) + 1
+        return 0
+
+
+if __name__ == "__main__":
+
+    if LOCAL_FILE:
+        # считываем входные данные из файла
+        input_data_path = Path(__file__).parent.resolve() / 'task_764_input.txt'
+        with open(input_data_path, 'r') as file:
+            # массив значений
+            nums = list(map(int, file.readline().split()))
+
+    else:
+        # считываем данные через терминал (для informatics)
+        # массив значений
+        nums = list(map(int, input().split()))
+
+    # инициализируем дерево
+    btree = Tree()
+
+    # добавляем вершины в дерево
+    for num in nums:
+        if num == 0:
+            break
+        
+        btree.add(key=num)
+    btree.check_balance()
+    print("YES")
